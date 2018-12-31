9f75f0be4f12a5a3071547967625f6db71795cc9
Roger Tan <teochewthunder@gmail.com>
2018-12-31 16:18:46 +0800

    Update cny2019.js

--- cny2019.js
+++ cny2019.js
@@ -25,12 +25,21 @@ function createPig()
 {
 	var body = document.createElement("div");
 	body.style.width = "100px";
-	body.style.height = "80px";
+	body.style.height = "90px";
 	body.style.borderRadius = "50%";
 	body.style.position = "relative";
 	body.style.backgroundColor = "rgba(255,100,100,1)";
 	body.style.float = "right";
 	
+	var snout = document.createElement("div");
+	snout.style.width = "60px";
+	snout.style.height = "50px";
+	snout.style.borderRadius = "50%";
+	snout.style.position = "relative";
+	snout.style.backgroundColor = "rgba(200,50,50,1)";
+	snout.style.margin = "10px auto 0 auto";
+	
+	body.appendChild(snout);
 	return body;
 }
 
