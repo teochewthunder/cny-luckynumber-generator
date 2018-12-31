f87d35f3e24603ea24995b14b6c8be220d916756
Roger Tan <teochewthunder@gmail.com>
2018-12-31 14:53:27 +0800

    Create cny2019.js

---
+++ cny2019.js
@@ -0,0 +1,34 @@
+function cny2019()
+{
+	var overlay = createOverlay();
+	var body = document.getElementByTagName("body");
+
+	body.appendChild(overlay);
+}
+
+function createOverlay()
+{
+	var div = document.createElement("div");
+	div.style.width = "100%";
+	div.style.height = 100px";
+	div.style.position = "absolute";
+	div.style.color = "black";
+	div.style.left = "0";
+	div.style.bottom = "0";
+	return div;
+}
+
+function createPig()
+{
+
+}
+
+function createCaption()
+{
+
+}
+
+function createSpeech()
+{
+
+}
