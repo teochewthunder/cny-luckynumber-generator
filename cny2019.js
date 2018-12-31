42c1b04dff6d350114ee289e3ff3db1bf6cbf673
Roger Tan <teochewthunder@gmail.com>
2018-12-31 15:46:18 +0800

    Update cny2019.js

--- cny2019.js
+++ cny2019.js
@@ -1,34 +1,34 @@
-function cny2019()
-{
-	var overlay = createOverlay();
-	var body = document.getElementByTagName("body");
+			function cny2019()
+			{
+				var overlay = createOverlay();
+				var body = document.getElementsByTagName("body");
 
-	body.appendChild(overlay);
-}
+				body[0].appendChild(overlay);
+			}
 
-function createOverlay()
-{
-	var div = document.createElement("div");
-	div.style.width = "100%";
-	div.style.height = 100px";
-	div.style.position = "absolute";
-	div.style.color = "black";
-	div.style.left = "0";
-	div.style.bottom = "0";
-	return div;
-}
+			function createOverlay()
+			{
+				var div = document.createElement("div");
+				div.style.width = "200px";
+				div.style.height = "100px";
+				div.style.position = "absolute";
+				div.style.backgroundColor = "rgba(0,0,0,0.3)";
+				div.style.right = "0";
+				div.style.bottom = "0";
+				return div;
+			}
 
-function createPig()
-{
+			function createPig()
+			{
 
-}
+			}
 
-function createCaption()
-{
+			function createCaption()
+			{
 
-}
+			}
 
-function createSpeech()
-{
+			function createSpeech()
+			{
 
-}
+			}
